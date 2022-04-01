e2633c21ffd2216236b5b9fdc79a7bdede0f1f65
Pavlo <ravliv.pavlobohdan@gmail.com>
2022-04-01 18:17:13 +0200

    feat: add repo from template

--- scripts/manag-github.ts
+++ scripts/manag-github.ts
@@ -1,8 +1,14 @@
 // import "@johnlindquist/kit";
 
 import { keyCodeFromKey } from "@johnlindquist/kit/core/keyboard";
-import { Octokit } from "octokit";
+import { Octokit } from "@octokit/rest";
 import config from "../config";
+
+// from template or fresh repository
+const freshRepo = await arg("fresh repository?:", [
+  { name: "fresh", description: "fresh", value: "fresh" },
+  { name: "fromTemplate", description: "not fresh", value: "not-fresh" },
+]);
 
 const repositoryName = await arg("Repository Name:");
 
@@ -16,10 +22,38 @@ const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 // await octokit.request("POST /user/repos", {
 //   name: repositoryName,
 //   description: repositoryName,
+//   private: true,
 // });
 
-await exec(
-  `cd ${config.homeDirPath} && git clone https://github.com/pawk3k/${repositoryName}.git `
-);
+// await exec(
+//   `cd ${config.homeDirPath} && git clone https://github.com/pawk3k/${repositoryName}.git `
+// );
 
-edit(`${config.homeDirPath}/${repositoryName}`);
+// edit(`${config.homeDirPath}/${repositoryName}`);
+if (freshRepo === "not-fresh") {
+  // create new repository using  template
+
+  // await octokit.request(
+  //   "POST /repos/ryansonshine/typescript-npm-package-template/generate",
+  //   {
+  //     template_owner: "ryansonshine",
+  //     template_repo: "typescript-npm-package-template",
+  //     name: repositoryName,
+  //   }
+  // );
+  await octokit.repos.createUsingTemplate({
+    template_owner: "ryansonshine",
+    template_repo: "typescript-npm-package-template",
+    name: repositoryName,
+    private: true,
+  });
+  await octokit.request(
+    "POST /repos/pawk3k/typescript-npm-package-template/generate",
+    {
+      template_owner: "pawk3k",
+      template_repo: "typescript-npm-package-template",
+      name: repositoryName,
+      headers: {},
+    }
+  );
+}
